41d6148efb9eb4df090422c79320fe7dd0c2c05d
Radek Šťasta <radek.stasta@gmail.com>
2025-09-22 12:59:02 +0200

    Removed FocusManager, all will be done in TileMapLayerHighlight

--- Scripts/focus_manager.gd
+++
@@ -1,6 +0,0 @@
-## Manages currently focused [Tile] and focus changes
-extends TileMapLayer
-class_name FocusManager
-
-## Currently focused [Tile] under mouse cursor (or null if no [Tile] is focused)
-var focused_tile: Tile = null
